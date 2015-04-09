4a59250ec5eaba7d55da6af5a0205675c7905733
Robin Cura <robin.cura@gmail.com>
2015-04-09 19:42:59 +0200

    typo

--- App1/app.R
+++ App1/app.R
@@ -13,12 +13,12 @@ load("../data/eurosex.RData")
 ##################
 
 server <- function(input, output) {
+  # Une fonction réactive qui renvoie un graphique
   output$plothist <- renderPlot({
-    hist(x = eurosex$FREQYEAR, # La variable
+    hist(x = eurosex$FREQYEAR,
          col = input$fillcol, 
          border = input$bordercol, 
          breaks = 5,
-          
          main = "Distribution de la fréquence moyenne des rapports",
          xlab = "Nombre moyen de rapports / an",
          ylab = "Fréquence")
@@ -34,21 +34,28 @@ ui <- fluidPage(
   # Titre de l'application
   titlePanel("Avec shiny, personnalise ton histogramme !"),
   
-  # Panneau latéral de contrôle
+  # Mise en page globale : Panneay latéral + page centrale
   sidebarLayout(
+    
+    # Panneau latéral de contrôle
     sidebarPanel(
+      
       selectInput(inputId = "fillcol", label = "Choisis la couleur de remplissage", 
                   selectize = TRUE, multiple = FALSE,
                   choices = c("black", "grey", "blue", "orange", "red")),
       selectInput(inputId = "bordercol", label = "Choisis la couleur de bordure", 
                   selectize = TRUE, multiple = FALSE,
                   choices = c("black", "grey", "blue", "orange", "red"))
-    ),
+    
+      ),
     
     # Panneau central
     mainPanel(
+      
       # Sortie du graphique
       plotOutput("plothist"),
+      
+      
       # Pour l'exercice...
       hr(),
       h3("Exercice"),
